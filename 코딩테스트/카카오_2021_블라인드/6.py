96d8b62779542832b6251da708b6669a23b7e17e
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2021-09-11 18:14:21 +0900

    6번 일차원 배열로 시도

--- 코딩테스트/카카오_2021_블라인드/6.py
+++ 코딩테스트/카카오_2021_블라인드/6.py
@@ -10,21 +10,24 @@
     [공격/회복, r1, c1, r2, c3, 정도]
 """
 from itertools import chain
+from collections import defaultdict
 
 def solution(board, skill):
-    r, c = len(board), len(board[0])
-    print(r, c)
-    # city = list(chain.from_iterable(board))
+    c = len(board[0])
+    city = list(chain.from_iterable(board))
+    attack = defaultdict(int)
 
     for act, r1, c1, r2, c2, degree in skill:
         if act == 2:
             degree = -degree
-        for i in range(r1, r2 + 1):
-            board[i][c1:c2+1] = map(lambda x: x - degree, board[i][c1:c2+1])
 
-    ans = 0
-    for row in board:
-        ans += len(list(filter(lambda x: x > 0, row)))
+        # for i in range(r1, r2+1):
+        #     city[i*r + c1 : i*r + c2 + 1] = map(lambda x: x - degree, city[i*r + c1 : i*r + c2 + 1])
+        city[r1 * c + c1: r2 * c + c2 + 1] = map(lambda x: x - degree, city[r1 * c + c1: r2 * c + c2 + 1])
+        print(city)
+
+    print(city)
+    ans = len(list(filter(lambda x: x > 0, city)))
 
     print(ans)
     return ans
@@ -36,4 +39,5 @@ def solution(board, skill):
 
 board = [[1,2,3],[4,5,6],[7,8,9]]
 skill = [[1,1,1,2,2,4],[1,0,0,1,1,2],[2,2,0,2,0,100]]
+
 solution(board, skill)
