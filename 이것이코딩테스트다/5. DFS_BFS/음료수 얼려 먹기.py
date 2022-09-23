486e771069ae1aebdb66f2be66b296101d04ab8c
Oh Dowon <dhehdnjs03@gmail.com>
2022-09-24 01:52:44 +0900

    bfs/dfs: 음료수 얼려먹기

--- 이것이코딩테스트다/5. DFS_BFS/음료수 얼려 먹기.py
+++ 이것이코딩테스트다/5. DFS_BFS/음료수 얼려 먹기.py
@@ -1,45 +1,51 @@
-"""
-    문제
-    1. n * m 얼음틀
-    2. 0 : 구멍
-    3. 1 : 칸막이
-    4. 아이스크림 개수구하기
+from collections import deque
 
-    입력조건
-    1. 세로길이 N -> 행 개수 N
-    2. 가로길이 M -> 열 개수 M
-
-    출력조건
-    1. 가능한 아이스크림 개수 출력
-
-    알고리즘
-    1. DFS 탐색
-        -> 탐색 끊기면 아이스크림 한개
-"""
 n, m = map(int, input().split())
-ice_cube = [list(input()) for _ in range(n)]
+tray = [list(input()) for _ in range(n)]
+print(n, m)
+print(tray)
 
-# 상하좌우 : (-1, 0), (1, 0), (0, -1), (0, 1)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(ice_cube, x, y):
+def bfs(r, c):
+    q = deque([(r, c)])
+
+    count = 0
+    while q:
+        x, y = q.popleft()
+
+        if tray[x][y] == "0":
+            count += 1
+            tray[x][y] = "#"
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
+            if tray[nx][ny] == "0":
+                q.append((nx, ny))
+
+    if count > 0:
+        return True
+    return False
+
+def dfs(x, y):
     if x < 0 or y < 0 or x >= n or y >= m:
         return False
 
-    if ice_cube[x][y] == "0":
-        ice_cube[x][y] = "#"
-
+    if tray[x][y] == "0":
+        tray[x][y] = "1"
         for i in range(4):
-            dfs(ice_cube, x + dx[i], y + dy[i])
-        return True
+            dfs(x + dx[i], y + dy[i])
 
+        return True
     return False
 
-ans = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(ice_cube, i, j) == True:
-            ans += 1
-
-print(ans)
+ice = 0
+for r in range(n):
+    for c in range(m):
+        if dfs(r, c):
+            ice += 1
+print(ice)
