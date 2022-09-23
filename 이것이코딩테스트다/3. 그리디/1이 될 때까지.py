153fff894d02be381e82f13d2d65bffd9d69a1a3
Oh Dowon <dhehdnjs03@gmail.com>
2022-09-23 22:15:10 +0900

    1이 될 때까지

--- 이것이코딩테스트다/3. 그리디/1이 될 때까지.py
+++ 이것이코딩테스트다/3. 그리디/1이 될 때까지.py
@@ -1,8 +1,24 @@
 n, k = map(int, input().split())
 
 count = 0
-while n >= k:
-    n //= k
-    count += 1
+# while n != 1:
+#     if n % k == 0:
+#         n //= k
+#     else:
+#         n -= 1
+#     count += 1
 
-print(count if n == 1 else count + (n - 1))
+while True:
+    target = (n // k) * k
+    count += (n - target)
+    n = target
+    if n < k:
+        break
+    count += 1
+    n //= k
+
+count += (n - 1)
+
+
+print(count)
+
