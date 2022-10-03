6cb3cdf6c68164f59b64371b382ef9027f48f14d
Oh Dowon <dhehdnjs03@gmail.com>
2022-10-03 21:19:08 +0900

    이진탐색 구현

--- 이것이코딩테스트다/7. 이진 탐색/binary_search.py
+++ 이것이코딩테스트다/7. 이진 탐색/binary_search.py
@@ -1,31 +1,23 @@
-def binary_search_recursion(array, target, start, end):
+def binary_search(arr, target, start, end):
     if start > end:
-        return None
+        return
 
     mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search_recursion(array, target, start, mid - 1)
-    else: # array[mid] < target
-        return binary_search_recursion(array, target, mid + 1, end)
 
-def binary_search_iterate(array, target, start, end):
+    if target == arr[mid]:
+        return mid
+
+    if arr[mid] > target:
+        return binary_search(arr, target, start, mid - 1)
+    return binary_search(arr, target, mid + 1, end)
+
+def binary_search_with_loop(arr, target, start, end):
     while start <= end:
         mid = (start + end) // 2
-        if array[mid] == target:
+        if target == arr[mid]:
             return mid
-        elif array[mid] > target:
+
+        if arr[mid] > target:
             end = mid - 1
-        else: # array[mid] < target
+        else:
             start = mid + 1
-    return None
-
-n, target = list(map(int, input().split()))
-array = list(map(int, input().split()))
-
-result = binary_search_iterate(array, target, 0, n-1)
-if result == None:
-    print("원소가 존재하지 않습니다.")
-else:
-    print(result)
