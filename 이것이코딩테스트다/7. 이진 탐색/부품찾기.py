f79200f802e155cabe84b5e97cd096f011bb0552
Oh Dowon <dhehdnjs03@gmail.com>
2022-10-03 21:55:14 +0900

    부품찾기

--- 이것이코딩테스트다/7. 이진 탐색/부품찾기.py
+++ 이것이코딩테스트다/7. 이진 탐색/부품찾기.py
@@ -1,26 +1,34 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 total = list(map(int, input().split()))
 total.sort()
 
 m = int(input())
-request = list(map(int, input().split()))
+items = list(map(int, input().split()))
 
-def binary_search(array, target, start, end):
+def binary_search(arr, target, start, end):
     while start <= end:
         mid = (start + end) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
+        if arr[mid] == target:
+            return True
+        if arr[mid] > target:
             end = mid - 1
-        else: # array[mid] < target
+        else:
             start = mid + 1
-    return None
+    return False
 
-for target in request:
-    if binary_search(total, target, 0, n-1) == None:
-        print("no")
+check_list = [0] * 1000001
+for number in total:
+    check_list[number] += 1
+
+answer = []
+for item in items:
+    # if binary_search(total, item, 0, n - 1):
+    #     answer.append("yes")
+    # else:
+    #     answer.append("no")
+    if check_list[item]:
+        answer.append('yes')
     else:
-        print("yes")
+        answer.append('no')
+
+print(*answer)
