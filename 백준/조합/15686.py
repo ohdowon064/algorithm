df80679a04c431a2fcfb0114919e8b7791334f57
Dowon Oh <dhehdnjs03@gmail.com>
2023-10-12 23:46:23 +0900

    두번풀기

--- 백준/조합/15686.py
+++ 백준/조합/15686.py
@@ -1,50 +1,45 @@
 n, m = map(int, input().split())
+chickens = []
+houses = []
 
-chicken_locations = []
-house_locations = []
-city = []
 for i in range(n):
-    elements = list(map(int, input().split()))
-    city.append(elements)
+    row = list(map(int, input().split()))
     for j in range(n):
-        if elements[j] == 2:
-            chicken_locations.append((i, j))
-        elif elements[j] == 1:
-            house_locations.append((i, j))
-
+        if row[j] == 1:
+            houses.append((i, j))
+        elif row[j] == 2:
+            chickens.append((i, j))
 
 def dist(r1, c1, r2, c2):
     return abs(r1 - r2) + abs(c1 - c2)
 
-
-def get_min_dist(comb):
-    min_dist = 0
-    for house in house_locations:
+min_dist = float('inf')
+def calc_min_dist(comb):
+    _min_dist = 0
+    for house in houses:
         my_min_dist = float('inf')
         for chicken in comb:
             my_min_dist = min(my_min_dist, dist(*house, *chicken))
+        _min_dist += my_min_dist
 
-        min_dist += my_min_dist
-    return min_dist
+    return _min_dist
+
 
 
 def dfs(comb: list, depth: int):
-    global final_min_dist
-
+    global min_dist
     if len(comb) == m:
-        final_min_dist = min(final_min_dist, get_min_dist(comb))
+        min_dist = min(min_dist, calc_min_dist(comb))
         return
 
-    if depth == len(chicken_locations):
+    if depth == len(chickens):
         return
 
-    comb.append(chicken_locations[depth])
+    comb.append(chickens[depth])
     dfs(comb, depth + 1)
     comb.pop()
     dfs(comb, depth + 1)
 
 
-final_min_dist = float('inf')
 dfs([], 0)
-
-print(final_min_dist)
+print(min_dist)
