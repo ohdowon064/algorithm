6d1dbd9cc8cd9695c4afa851de09c53a2d78fbf3
Dowon Oh <dhehdnjs03@gmail.com>
2023-10-13 18:20:54 +0900

    복습

--- 삼성기출/나선이동.py
+++ 삼성기출/나선이동.py
@@ -1,38 +1,39 @@
-# 나선이동
-# 중심에서 시작하여 방향을 2번 바꿀 때마다 이동거리가 1씩 증가한다.
 from pprint import pprint
 
 n = 5
 board = [[0] * n for _ in range(n)]
 
-# 이동 방향: 좌, 하, 우, 상
+# 이동방향: 좌하우상
 dr = [0, 1, 0, -1]
 dc = [-1, 0, 1, 0]
 
+def out_of_range(r, c):
+    return r < 0 or r >= n or c < 0 or c >= n
+
 def spiral_move():
-    r = c = n // 2 # 중앙 좌표
-    direction = move_count = number = 0
+    # 시작 좌표
+    r = c = n // 2
+    # 시작방향
+    d = 0
+    # 이동 카운트
+    move_count = 0
+    # 이동 거리
     dist = 1
 
     while True:
         move_count += 1
         for _ in range(dist):
-            nr, nc = r + dr[direction], c + dc[direction]
-
-            # 종료조건: 이동 좌표가 마지막 좌표 도착한 경우
-            # 좌하우상: (0, -1)
-            if (nr, nc) == (0, -1):
+            nr, nc = r + dr[d], c + dc[d]
+            if out_of_range(nr, nc):
                 return
-            number += 1
-            board[nr][nc] = number
 
+            board[nr][nc] = board[r][c] + 1
             r, c = nr, nc
 
+        d = (d + 1) % 4
         if move_count == 2:
             dist += 1
             move_count = 0
-
-        direction = (direction + 1) % 4
 
 spiral_move()
 pprint(board)
