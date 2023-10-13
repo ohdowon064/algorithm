41b65aae029d2d065bbfabb8e46e160d9517dc4c
Dowon Oh <dhehdnjs03@gmail.com>
2023-10-13 19:59:49 +0900

    부분배열 회전

--- 삼성기출/배열회전.py
+++ 삼성기출/배열회전.py
@@ -1,19 +1,30 @@
-# 2차원 배열 회전
-# 직사각형 회전
-# 부분배열 회전
+from copy import deepcopy
 from pprint import pprint
 
-n = 5
-board = [[i * n + j for j in range(1, n + 1)] for i in range(n)]
 
-def transpose():
+def transpose(board):
     return list(map(list, zip(*board)))
 
-def rotate(clockwise=True):
+def rotate(board, clockwise=True):
     if clockwise:
         return list(map(list, zip(*board[::-1])))
-    return transpose()[::-1]
+    return transpose(board)[::-1]
 
-pprint(transpose())
-pprint(rotate())
-pprint(rotate(clockwise=False))
+n = 5
+myboard = [[i * n + j for j in range(1, n + 1)] for i in range(n)]
+
+# pprint(transpose(myboard))
+# pprint(rotate(myboard))
+# pprint(rotate(myboard, clockwise=False))
+
+def rotate_sublist(board, xr, xc, yr, yc):
+    board = deepcopy(board)
+    sublist = [row[xc: yc + 1] for row in board[xr: yr + 1]]
+    sublist = rotate(sublist)
+    for i in range(xr, yr + 1):
+        for j in range(xc, yc + 1):
+            board[i][j] = sublist[i - xr][j - xc]
+
+    return board
+pprint(myboard)
+pprint(rotate_sublist(myboard, 1, 1, 3, 3))
